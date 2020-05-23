c28f64971a270e5cf970b5379852dc9f596a25af
Justin Huth <sgthuth@debian.mshome>
2020-05-23 02:00:42 -0400

    updated headline

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>Welcome to Tribal!</h1>
+        <p>-Justin</p>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
